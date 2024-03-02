781d617441ddf1fbbb7ef75f8794577ce8ca0653
felipesimao1 <felipe.simao1@gmail.com>
2024-03-02 11:34:35 +0000

    fixinh deploy2

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,46 +1,64 @@
 import random
 
-def choose_word():
-    """
-    Escolhe uma palavra aleatória para o jogo.
-    """
-    words = ["python", "banana", "computador", "elefante", "girafa", "programação"]
-    return random.choice(words)
+class BattleshipGame:
+    def __init__(self, size=5):
+        self.size = size
+        self.board = [[' ' for _ in range(size)] for _ in range(size)]
+        self.ships = size // 2
+        self.remaining_ships = self.ships
 
-def display_word(word, guessed_letters):
-    """
-    Exibe a palavra com letras não adivinhadas substituídas por '_'.
-    """
-    return ' '.join([letter if letter in guessed_letters else '_' for letter in word])
+    def place_ships(self):
+        for _ in range(self.ships):
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
+            self.board[x][y] = 'S'
 
-def hangman():
-    """
-    Função principal para o jogo da Forca.
-    """
-    print("Bem-vindo ao jogo da Forca!")
-    word = choose_word()
-    guessed_letters = set()
-    attempts = 6
+    def print_board(self, show_ships=False):
+        print("  " + " ".join(str(i) for i in range(self.size)))
+        for i in range(self.size):
+            row = " ".join(self.board[i][j] if self.board[i][j] != 'S' or show_ships else ' ' for j in range(self.size))
+            print(str(i) + " " + row)
 
-    while attempts > 0:
-        print("\nPalavra:", display_word(word, guessed_letters))
-        guess = input("Tente adivinhar uma letra: ").lower()
-
-        if guess in guessed_letters:
-            print("Você já tentou essa letra.")
-        elif guess in word:
-            print("Letra correta!")
-            guessed_letters.add(guess)
-            if set(word) == guessed_letters:
-                print("Parabéns! Você ganhou!")
-                break
+    def make_guess(self, x, y):
+        if self.board[x][y] == 'S':
+            print("Hit!")
+            self.board[x][y] = 'H'
+            self.remaining_ships -= 1
+            if self.remaining_ships == 0:
+                print("Congratulations! You sunk all the battleships.")
+                return True
+        elif self.board[x][y] == 'H' or self.board[x][y] == 'M':
+            print("You've already guessed this position.")
         else:
-            print("Letra errada.")
-            attempts -= 1
-            print("Tentativas restantes:", attempts)
-        
-    else:
-        print("Você perdeu! A palavra era:", word)
+            print("Miss!")
+            self.board[x][y] = 'M'
+        return False
+
+def get_valid_input(prompt, size):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 0 <= value < size:
+                return value
+            else:
+                print("Invalid input. Value must be between 0 and {}.".format(size - 1))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def main():
+    print("Welcome to Battleship Game!")
+    size = get_valid_input("Enter the size of the board (minimum size is 5): ", 100)
+    game = BattleshipGame(size)
+    game.place_ships()
+    game_over = False
+
+    while not game_over:
+        print("\nYour current board:")
+        game.print_board()
+        print("\nMake a guess:")
+        x = get_valid_input("Enter X coordinate: ", size)
+        y = get_valid_input("Enter Y coordinate: ", size)
+        game_over = game.make_guess(x, y)
 
 if __name__ == "__main__":
-    hangman()
+    main()
